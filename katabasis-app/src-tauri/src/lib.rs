ed6b859f4c2867558545bd0fad03f341245f3470
jack-whatley <wickselm2005@gmail.com>
2025-02-23 01:57:20 +0000

    Implementing App Updater

--- katabasis-app/src-tauri/src/lib.rs
+++ katabasis-app/src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri_plugin_updater::UpdaterExt;
 use manager::{Collection, Plugin, SupportedGames, SupportedPluginSources};
 
 #[tauri::command]
@@ -94,6 +95,21 @@ async fn import_collection(file_path: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(
+            |app| {
+                #[cfg(desktop)]
+                app.handle().plugin(tauri_plugin_updater::Builder::new().build())
+                    .expect("Failed to initialise updater");
+
+                let handle = app.handle().clone();
+
+                tauri::async_runtime::spawn(async move {
+                    update(handle).await.unwrap();
+                });
+
+                Ok(())
+            }
+        )
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
@@ -111,4 +127,25 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+
+        update.download_and_install(
+            |chunk_length, content_length| {
+                downloaded += chunk_length;
+                println!("downloaded {downloaded} from {content_length:?}");
+            },
+            || {
+                println!("download finished");
+            }
+        ).await?;
+
+        println!("update installed");
+        app.restart();
+    }
+
+    Ok(())
 }
